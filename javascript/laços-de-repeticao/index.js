fa730c695becaee80441354176e31c43a3428dab
joaoito <joaovictorpfr@gmail.com>
2024-10-09 19:51:09 -0300

    codigo lacos

---
+++ javascript/laços-de-repeticao/index.js
@@ -0,0 +1,8 @@
+function laçoDeRepeticao(saudacao){
+    for(let i = 0; i < 5; i++){
+        console.log("A minha variável de controle tem valor: " + i)
+        console.log(saudacao);
+    }
+}
+
+console.log(laçoDeRepeticao("Eae Higor!"));
